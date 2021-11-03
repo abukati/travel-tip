13eb6a127bffae87dee4f922d56d4f3c6d311601
BaselBoulos <baselboulos@gmail.com>
2021-11-03 18:45:28 +0200

    reviewing and learning the code, editing variables names

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,40 @@
 import { storageService } from './local-storage.service.js'
 import { makeId } from './utils.service.js'
 
-export const locService = { getLocs, panLoc, deleteLoc, askNewLocName, saveCurrLoc }
+export const locService = {
+  getLocs,
+  panLoc,
+  removeLoc,
+  askNewLocName,
+  saveLoc,
+}
 
-const STORAGE_KEY = 'locs'
-
+const STORAGE_KEY = 'locationsDB'
 const locs = storageService.loadFromStorage(STORAGE_KEY) || []
 
+function saveLoc(loc) {
+  askNewLocName()
+    .then((res) => {
+      let newLoc = {
+        id: makeId(),
+        name: res,
+        lat: loc.lat,
+        lng: loc.lng,
+        createdAt: Date().slice(0, 24),
+        updatedAt: Date().slice(0, 24),
+      }
+      locs.push(newLoc)
+      storageService.saveToStorage(STORAGE_KEY, locs)
+    })
+    .catch((err) => console.log(err))
+}
 
 function panLoc(name) {
   let loc = locs.find((loc) => loc.name === name)
   return Promise.resolve({ lat: loc.lat, lng: loc.lng })
 }
 
-function deleteLoc(name) {
+function removeLoc(name) {
   let locIdx = locs.findIndex((loc) => loc.name === name)
   locs.splice(locIdx, 1)
   storageService.saveToStorage(STORAGE_KEY, locs)
@@ -28,25 +49,9 @@ function getLocs() {
   })
 }
 
-function saveCurrLoc(loc) {
-  askNewLocName().then(res => {
-    let newLoc = {
-      id: makeId(),
-      name: res,
-      lat: loc.lat,
-      lng: loc.lng,
-      createdAt: Date().slice(0,24),
-      updatedAt: Date().slice(0,24)
-    }
-    locs.push(newLoc)
-    storageService.saveToStorage(STORAGE_KEY, locs)
-  })
-  .catch(err => console.log(err))
-}
-
 function askNewLocName() {
-  return new Promise((res, rej) => {
+  return new Promise((resolve, reject) => {
     const name = prompt('Name the spot')
-    return name ? res(name) : rej('no input')
+    return name ? resolve(name) : reject('no input')
   })
 }
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,76 +1,72 @@
 export const mapService = {
-    initMap,
-    addMarker,
-    panTo,
-    // map: gMap,
-    getMarkers,
-    getGeocode
+  initMap,
+  addMarker,
+  panTo,
+  getMarkers,
+  getGeocode,
 }
 
+const GOOGLE_API_KEY = 'AIzaSyBVA3c6L5XdP2nQhdQ2zLeXfoe7GJee8-I'
+
 let gMap
-
 let gMarkers = []
-
 let currPos
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    return _connectGoogleApi()
-        .then(() => {
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 10
-            })
-            let infoWindow = new google.maps.InfoWindow({
-                content: 'Find and click locations you love!',
-                position: { lat, lng }
-            })
-            infoWindow.open(gMap)
-            gMap.addListener('click', ev => {
-                infoWindow.close()
-                const coords = {
-                    lat: ev.latLng.lat(),
-                    lng: ev.latLng.lng()
-                }
-                infoWindow = new google.maps.InfoWindow({
-                    position: coords
-                })
-                infoWindow.setContent(JSON.stringify(ev.latLng.toJSON(), null, 2))
-                infoWindow.open(gMap)
-            })
-        })
-}
-
-function addMarker(loc) {
-    var marker = new google.maps.Marker({
-        position: loc,
-        map: gMap,
+  const elMap = document.querySelector('#map')
+  return _connectGoogleApi().then(() => {
+    gMap = new google.maps.Map(elMap, { center: { lat, lng }, zoom: 10 })
+    let infoWindow = new google.maps.InfoWindow({
+      content: 'Find and click locations you love!',
+      position: { lat, lng },
     })
-    gMarkers.push(marker)
-    return marker
+    infoWindow.open(gMap)
+    gMap.addListener('click', (ev) => {
+      infoWindow.close()
+      const coords = {
+        lat: ev.latLng.lat(),
+        lng: ev.latLng.lng(),
+      }
+      infoWindow = new google.maps.InfoWindow({
+        content: `${JSON.stringify(coords)}`,
+        position: coords,
+      })
+      infoWindow.open(gMap)
+    })
+  })
 }
 
 function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng(lat, lng)
-    gMap.panTo(laLatLng)
-    addMarker(laLatLng)
+  var laLatLng = new google.maps.LatLng(lat, lng)
+  gMap.panTo(laLatLng)
+  addMarker(laLatLng)
+}
+
+function addMarker(pos) {
+  var marker = new google.maps.Marker({
+    position: pos,
+    map: gMap,
+  })
+  gMarkers.push(marker)
+  return marker
 }
 
 function getMarkers() {
-    return Promise.resolve(gMarkers)
+  return Promise.resolve(gMarkers)
 }
 
-function getGeocode(searchValue) {
-    const API_KEY = 'AIzaSyBVA3c6L5XdP2nQhdQ2zLeXfoe7GJee8-I'
-    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${searchValue}&key=${API_KEY}`)
-        .then(res => res.data.results[0].geometry.location)
+function getGeocode(location) {
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => res.data.results[0].geometry.location)
 }
 
 function _connectGoogleApi() {
   if (window.google) return Promise.resolve()
-  const API_KEY = 'AIzaSyBVA3c6L5XdP2nQhdQ2zLeXfoe7GJee8-I'
   var elGoogleApi = document.createElement('script')
-  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
+  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`
   elGoogleApi.async = true
   document.body.append(elGoogleApi)
 
