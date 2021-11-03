f100a60d46815ca5b634e813ecd667300f9e2b14
BaselBoulos <baselboulos@gmail.com>
2021-11-03 18:58:08 +0200

    fixing conflicts

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -49,24 +49,6 @@ function getLocs() {
   })
 }
 
-function saveLoc(loc) {
-  askNewLocName()
-    .then((res) => {
-      let newLoc = {
-        id: makeId(),
-        name: res,
-        lat: loc.lat(),
-        lng: loc.lng(),
-        createdAt: Date().slice(0, 24),
-        updatedAt: Date().slice(0, 24),
-      }
-      console.log(newLoc)
-      locs.push(newLoc)
-      storageService.saveToStorage(STORAGE_KEY, locs)
-    })
-    .catch((err) => console.log(err))
-}
-
 function askNewLocName() {
   return new Promise((resolve, reject) => {
     const name = prompt('Name the spot')
